96da8ef74bdd5d0e9d497447f37959043dff0e07
Juan Martin Restrepo <tinto922@gmail.com>
2022-08-08 11:59:25 -0400

    update

--- src/main/java/com/example/javamod6springweblab1/dto/CreateCamperDTO.java
+++ src/main/java/com/example/javamod6springweblab1/dto/CreateCamperDTO.java
@@ -2,8 +2,15 @@ package com.example.javamod6springweblab1.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class CreateCamperDTO {
+    @NotBlank
     private String name;
+    @Min(value = 8)
+    @Max(18)
     private Integer age;
 }
--- src/main/java/com/example/javamod6springweblab1/dto/CreateSignupDTO.java
+++ src/main/java/com/example/javamod6springweblab1/dto/CreateSignupDTO.java
@@ -2,8 +2,13 @@ package com.example.javamod6springweblab1.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 @Data
 public class CreateSignupDTO {
+    @Min(value = 0)
+    @Max(23)
     private Integer time;
     private Integer camper_id;
     private Integer activity_id;
